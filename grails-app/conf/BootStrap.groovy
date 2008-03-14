9656ffb184ac1246128b78bed4f1bf4bf94d5369
ductrung.tran <ductrung.tran@97a2b959-5948-0410-8930-3fb01bf48abf>
2008-03-14 14:36:30 +0000

    exclude folder from list

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,11 +1,12 @@
 class BootStrap {
 
      def init = { servletContext ->
-         new File(servletContext.getRealPath("/_photos")).list().each {name ->
-             if (new File(name).isFile())
-                new Photo(name : name, description : name, url : name).save()
+         new File(servletContext.getRealPath("/_photos")).listFiles().each {file ->
+         		if (file.isFile())
+         			new Photo(name : file.name, description : file.name, url : file.name).save()
+
      	}
      }
      def destroy = {
      }
-} 
+}
